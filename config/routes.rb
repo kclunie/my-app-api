4424500b333e42c75017839be80b2d9a4fd46bce
Kristen Clunie <kcluniesa@yahoo.com>
2020-09-14 16:20:11 -0400

    add route relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :lists
-  resources :cards
+  resources :cards do
+    resources :lists
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
